e8d3b8c7e80e3d92e5f234ce8ef27a17639c55f4
EthanShin <avoirom@gmail.com>
2018-08-12 16:34:02 +0900

    feat: Modify Forming_a_Magic_Square.kt to include new feature
    date: 2018-08-12
    description:
    Given a 3*3 matrix of integers, convert it into a magic square at
    minimal cost.
    
    1. Create all magic squares of 3 * 3 matrix.
    2. Find out the difference between the elements of each magic square and the input matrix.
    3. Add the value obtained in step 2 for each magic square.
    4. Print the smallest value among the values obtained in step 3.

--- ethan/Algorithms/Implementation-Forming_a_Magic_Square.kt
+++ ethan/Algorithms/Implementation-Forming_a_Magic_Square.kt
@@ -8,7 +8,31 @@ import kotlin.text.*
 // Complete the formingMagicSquare function below.
 fun formingMagicSquare(s: Array<Array<Int>>): Int {
 
-    return 0
+    val base = arrayOf(
+            arrayOf(arrayOf(8, 1, 6), arrayOf(3, 5, 7), arrayOf(4, 9, 2)),
+            arrayOf(arrayOf(4, 3, 8), arrayOf(9, 5, 1), arrayOf(2, 7, 6)),
+            arrayOf(arrayOf(6, 7, 2), arrayOf(1, 5, 9), arrayOf(8, 3, 4)),
+            arrayOf(arrayOf(2, 9, 4), arrayOf(7, 5, 3), arrayOf(6, 1, 8)),
+            arrayOf(arrayOf(4, 9, 2), arrayOf(3, 5, 7), arrayOf(8, 1, 6)),
+            arrayOf(arrayOf(2, 7, 6), arrayOf(9, 5, 1), arrayOf(4, 3, 8)),
+            arrayOf(arrayOf(8, 3, 4), arrayOf(1, 5, 9), arrayOf(6, 7, 2)),
+            arrayOf(arrayOf(6, 1, 8), arrayOf(7, 5, 3), arrayOf(2, 9, 4))
+    )
+
+    var sum = Array(8) { 0 }
+
+    for (i in 0 until 3) {
+
+        for (j in 0 until 3) {
+
+            for (k in 0 until 8) {
+                sum[k] += abs(s[i][j] - base[k][i][j])
+            }
+        }
+    }
+    sum.sort()
+
+    return sum.first()
 }
 
 fun main(args: Array<String>) {
