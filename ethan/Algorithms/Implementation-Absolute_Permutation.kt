fbd720e9a1c75f890752d744c93ed5cc3c0d543e
EthanShin <avoirom@gmail.com>
2018-08-14 23:35:04 +0900

    feat: Modify Absolute_Permutation.kt to include new feature
    date: 2018-08-14
    description:
    Given a range of the array, print the lexicographically smallest
    absolute permutation.
    
    1. If k is 0, print the number from 1 to n.
    2. If n is not divided by k, or n is not an even number divided by k,
    print -1.
    3-1. Perform steps 3-2 and 3-3 while changing i from 1 to n.
    3-2. If i/k%2==0, print i+1+k.
    3-3. Else, print i+1-k.

--- ethan/Algorithms/Implementation-Absolute_Permutation.kt
+++ ethan/Algorithms/Implementation-Absolute_Permutation.kt
@@ -6,7 +6,27 @@ import kotlin.text.*
 // Complete the absolutePermutation function below.
 fun absolutePermutation(n: Int, k: Int): Array<Int> {
 
-    return arrayOf(0)
+    var result = Array(n) {0}
+
+    if (k == 0) {
+        for (i in 1..n) {
+            result[i-1] = i
+        }
+        return result
+
+    } else if (n % k != 0 || n / k % 2 != 0) {
+        return arrayOf(-1)
+    }
+
+    for (i in 0 until n) {
+        if ((i / k) % 2 == 0) {
+            result[i] = i+1 + k
+        } else {
+            result[i] = i+1 - k
+        }
+    }
+
+    return result
 }
 
 fun main(args: Array<String>) {
