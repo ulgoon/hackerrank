d64c4952cad4692391424a0f6cf33a43582b6b17
EthanShin <avoirom@gmail.com>
2018-08-16 23:56:36 +0900

    feat: Modify Almost_Sorted.kt to include new feature
    date: 2018-08-16
    description:
    Given an array of integers, sort by selecting either one swap or one
    reverse.
    
    1-1. Find the index out of order by searching the array from the
    beginning.
    1-2. Find the index out of order by searching the array from the
    end.
    2. Swap the above two indexes.
    3. Check that the array is in ascending order.

--- ethan/Algorithms/Implementation-Almost_Sorted.kt
+++ ethan/Algorithms/Implementation-Almost_Sorted.kt
@@ -5,6 +5,34 @@ import kotlin.text.*
 // Complete the almostSorted function below.
 fun almostSorted(arr: Array<Int>): Unit {
 
+    var sortedArr = arr.copyOf()
+    sortedArr.sort()
+
+    var first = 0
+    var last = arr.size-1
+
+    for (i in 0 until arr.size-1) {
+        if (arr[i] > arr[i + 1]) {
+            first = i
+            break
+        }
+    }
+
+    for (j in arr.size-1 downTo 1) {
+        if (arr[j - 1] > arr[j]) {
+            last = j
+            break
+        }
+    }
+
+    arr[first] = arr[last].also { arr[last] = arr[first] }
+
+    if (Arrays.toString(sortedArr) == Arrays.toString(arr)) {
+        println("yes")
+        println("swap ${first+1} ${last+1}")
+    } else {
+        println("no")
+    }
 }
 
 fun main(args: Array<String>) {
