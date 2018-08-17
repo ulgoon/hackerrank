d27b7e716ae70d6ce61db7f66f80bc11fa045ded
EthanShin <avoirom@gmail.com>
2018-08-17 19:40:57 +0900

    feat: Modify Almost_Sorted.kt to include new feature
    date: 2018-08-17
    description:
    Given an array of integers, sort by selecting either one swap or one
    reverse.
    
    1. Reverse the index from first + 1 to last in the array.
    2. Check that the array is in ascending order.

--- ethan/Algorithms/Implementation-Almost_Sorted.kt
+++ ethan/Algorithms/Implementation-Almost_Sorted.kt
@@ -14,15 +14,25 @@ fun almostSorted(arr: Array<Int>): Unit {
     while (first < arr.size - 2 && arr[first] < arr[first + 1]) first++
     while (last > 0 && arr[last - 1] < arr[last]) last--
 
+    // swap
     arr[first] = arr[last].also { arr[last] = arr[first] }
 
     if (Arrays.toString(sortedArr) == Arrays.toString(arr)) {
         println("yes")
         println("swap ${first+1} ${last+1}")
-    } else {
-        println(Arrays.toString(arr))
-        println("no")
+        return
     }
+
+    // reverse
+    arr.sort(first+1, last)
+
+    if (Arrays.toString(sortedArr) == Arrays.toString(arr)) {
+        println("yes")
+        println("reverse ${first+1} ${last+1}")
+        return
+    }
+
+    println("no")
 }
 
 fun main(args: Array<String>) {
