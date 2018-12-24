7f47af7660c413b418ad6f6496140b0c91af3010
EthanShin <avoirom@gmail.com>
2018-12-24 23:34:33 +0900

    feat: Modify Queens_Attack.kt only if there is no obstacle
    date: 2018-12-24
    description:
    Given the queen's position and the locations of all the obstacles, find and print the number of squares the queen can attack.
    
    1. If there is no obstacles, outputs in the space of 8 directions.

--- ethan/Algorithms/Implementation-Queens_Attack.kt
+++ ethan/Algorithms/Implementation-Queens_Attack.kt
@@ -6,7 +6,17 @@ import kotlin.text.*
 
 fun queensAttack(n: Int, k: Int, r_q: Int, c_q: Int, obstacles: Array<Array<Int>>): Int {
 
-    return 0
+    var up = n - r_q
+    var down = r_q - 1
+    var left = c_q - 1
+    var right = n - c_q
+
+    var up_left = minOf(up, left)
+    var down_left = minOf(down, left)
+    var down_right = minOf(down, right)
+    var up_right = minOf(up, right)
+
+    return up + down + left + right + up_left + down_left + down_right + up_right
 }
 
 fun main(args: Array<String>) {
@@ -20,7 +30,7 @@ fun main(args: Array<String>) {
     val r_q = r_qC_q[0].trim().toInt()
     val c_q = r_qC_q[1].trim().toInt()
 
-    val obstacles = Array(k) { Array(2) { 0 } }
+    val obstacles = Array<Array<Int>>(k) { Array<Int>(2) { 0 } }
 
     for (i in 0 until k) {
         obstacles[i] = scan.nextLine().split(" ").map{ it.trim().toInt() }.toTypedArray()
