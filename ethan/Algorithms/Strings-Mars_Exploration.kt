a6ab10d354edb9c52e45a6041c5cd209b4291c27
EthanShin <avoirom@gmail.com>
2019-02-02 23:38:19 +0900

    feat: Add Strings-Mars_Exploration.kt
    description:
    Print the wrong number of characters in a series of SOS messages.

---
+++ ethan/Algorithms/Strings-Mars_Exploration.kt
@@ -0,0 +1,15 @@
+import java.util.*
+import kotlin.io.*
+
+fun marsExploration(s: String): Int {
+
+    return 0
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+    val s = scan.nextLine()
+
+    val result = marsExploration(s)
+    println(result)
+}
