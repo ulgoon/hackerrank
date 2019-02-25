82cd747941ccdd817dde844b54a81d8f5bcf9dc1
EthanShin <avoirom@gmail.com>
2019-02-25 23:51:38 +0900

    feat: Modify Alternating_Characters.kt
    description:
    If adjacent characters have the same value, the characters are deleted and only one is left. And outputs the deleted number.
    
    Timeout in some cases.

--- ethan/Algorithms/Strings-Alternating_Characters.kt
+++ ethan/Algorithms/Strings-Alternating_Characters.kt
@@ -4,7 +4,19 @@ import kotlin.text.*
 
 fun alternatingCharacters(s: String): Int {
 
-    return 0
+    var mString = s
+    var result = 0
+
+    var i = 0
+    while (i <= mString.length-2) {
+        if (mString[i] == mString[i+1]) {
+            mString = mString.removeRange(i+1, i+2)
+            result++
+        } else {
+            i++
+        }
+    }
+    return result
 }
 
 fun main(args: Array<String>) {
