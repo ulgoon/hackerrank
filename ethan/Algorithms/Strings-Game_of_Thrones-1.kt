b2a6a212e6c9bb671a7a09013f1b9f54e4692819
EthanShin <avoirom@gmail.com>
2019-02-28 23:28:40 +0900

    feat: Add Game_of_Thrones-1.kt
    description:
    How can I rearrange the input characters to create a palindrome?

---
+++ ethan/Algorithms/Strings-Game_of_Thrones-1.kt
@@ -0,0 +1,17 @@
+import java.util.*
+import kotlin.io.*
+
+fun gameOfThrones(s: String): String {
+
+    return ""
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val s = scan.nextLine()
+
+    val result = gameOfThrones(s)
+
+    println(result)
+}
