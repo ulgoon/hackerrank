03bb75f8eb3dc3ea02cbb5fa2a7b25167982d0a1
EthanShin <avoirom@gmail.com>
2019-03-02 23:41:36 +0900

    feat: Add Two_Strings.kt
    description:
    Is there a character common to the two strings entered?

---
+++ ethan/Algorithms/Strings-Two_Strings.kt
@@ -0,0 +1,22 @@
+import java.util.*
+import kotlin.io.*
+import kotlin.text.*
+
+fun twoStrings(s1: String, s2: String): String {
+
+    return "NO"
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val q = scan.nextLine().trim().toInt()
+
+    for (qItr in 1..q) {
+        val s1 = scan.nextLine()
+        val s2 = scan.nextLine()
+
+        val result = twoStrings(s1, s2)
+        println(result)
+    }
+}
