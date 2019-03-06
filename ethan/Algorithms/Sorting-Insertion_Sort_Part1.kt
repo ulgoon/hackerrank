476726f13f36f9cbe58bf0b6040ab62e819bff52
EthanShin <avoirom@gmail.com>
2019-03-06 23:20:48 +0900

    feat: Modify Insertion_Sort_Part1.kt
    description:
    Write a Insertion Sort.
    
    1. After the index has been exchanged, it is output.
    2. Finally, the value in the target is put in the first index and output.

--- ethan/Algorithms/Sorting-Insertion_Sort_Part1.kt
+++ ethan/Algorithms/Sorting-Insertion_Sort_Part1.kt
@@ -4,6 +4,15 @@ import kotlin.text.*
 
 fun insertionSort1(n: Int, arr: Array<Int>): Unit {
 
+    var i = n - 1
+    var target = arr[i]
+    while (i > 0 && arr[i-1] > target) {
+        arr[i] = arr[i-1]
+        println(arr.joinToString(' '.toString()))
+        i--
+    }
+    arr[i] = target
+    println(arr.joinToString(' '.toString()))
 }
 
 fun main(args: Array<String>) {
