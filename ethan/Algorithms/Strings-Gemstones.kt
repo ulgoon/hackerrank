935e301de61019659cc4577f3ca077256a516d69
EthanShin <avoirom@gmail.com>
2019-03-20 23:57:21 +0900

    feat: Modify Gemstones.kt
    description:
    Print the number of characters that are common to given strings.
    
    Express the problem solving process as a comment.

--- ethan/Algorithms/Strings-Gemstones.kt
+++ ethan/Algorithms/Strings-Gemstones.kt
@@ -4,7 +4,13 @@ import kotlin.ranges.*
 import kotlin.text.*
 
 fun gemstones(arr: Array<String>): Int {
-    
+
+    // 1. 문자열.toSet()로 갖고 있는 문자를 찾는다.
+
+    // 2. intersect()를 이용하여 공통된 문자를 찾는다.
+
+    // 3. 그 개수를 출력한다.
+
     return 0
 }
 
