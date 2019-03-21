a167dfe8c26464e3e8cdc38dc52bde68323f7798
EthanShin <avoirom@gmail.com>
2019-03-21 23:54:45 +0900

    feat: Modify Gemstones.kt
    description:
    Print the number of characters that are common to given strings.
    
    1. Use set collection to remove duplicate characters from a string.
    2. Use the intersect function to find duplicate elements with other strings.
    3. Prints the size of the result.

--- ethan/Algorithms/Strings-Gemstones.kt
+++ ethan/Algorithms/Strings-Gemstones.kt
@@ -4,14 +4,17 @@ import kotlin.ranges.*
 import kotlin.text.*
 
 fun gemstones(arr: Array<String>): Int {
+    
+    var result = arr[0].toSet()
 
     // 1. 문자열.toSet()로 갖고 있는 문자를 찾는다.
-
     // 2. intersect()를 이용하여 공통된 문자를 찾는다.
+    for (s in arr) {
+        result = result.intersect(s.toSet())
+    }
 
     // 3. 그 개수를 출력한다.
-
-    return 0
+    return result.size
 }
 
 fun main(args: Array<String>) {
