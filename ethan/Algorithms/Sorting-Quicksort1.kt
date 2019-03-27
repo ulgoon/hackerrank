959acbaf6f3e96361a9566390eea9ebdc38e7652
EthanShin <avoirom@gmail.com>
2019-03-27 23:10:14 +0900

    feat: Add Quicksort1.kt
    description:
    Use quicksort to output the results of one round of progress.

---
+++ ethan/Algorithms/Sorting-Quicksort1.kt
@@ -0,0 +1,19 @@
+import java.util.*
+import kotlin.collections.*
+import kotlin.io.*
+import kotlin.text.*
+
+fun quickSort(arr: Array<Int>): Array<Int> {
+
+    return arrayOf(0)
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val n = scan.nextLine().trim().toInt()
+    val arr = scan.nextLine().split(" ").map{ it.trim().toInt() }.toTypedArray()
+
+    val result = quickSort(arr)
+    println(result.joinToString(" "))
+}
