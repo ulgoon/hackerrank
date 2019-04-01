97975c941865b626e9ded998741e733f3a1b5f29
EthanShin <avoirom@gmail.com>
2019-04-01 23:13:47 +0900

    feat: Modify Counting_Sort2.kt
    description:
    Write the counting sort.

--- ethan/Algorithms/Sorting-Counting_Sort2.kt
+++ ethan/Algorithms/Sorting-Counting_Sort2.kt
@@ -5,7 +5,23 @@ import kotlin.text.*
 
 fun countingSort(arr: Array<Int>): Array<Int> {
 
-    return arrayOf(0)
+    // 1. 숫자가 0 ~ 99로 고정이라는 조건에 따라 size가 100인 배열 생성
+    var count = Array(100) {0}
+
+    // 2. 입력받은 숫자를 확인하여 해당 배열값 증가
+    arr.forEach { count[it]++ }
+
+    // 3. 결과를 저장할 리스트 생성
+    var result = mutableListOf<Int>()
+
+    // 4. count 배열의 값을 순차적으로 확인
+    count.forEachIndexed { index, i ->
+        // 4-1. 배열 index 값을 i만큼 리스트에 저장
+        (0 until i).forEach { result.add(index) }
+    }
+
+    // 5. 리스트 출력
+    return result.toTypedArray()
 }
 
 fun main(args: Array<String>) {
