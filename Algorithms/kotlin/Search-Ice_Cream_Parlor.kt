7761a92e8a6f484d914593ce74bd3c0cb50ce647
EthanShin <avoirom@gmail.com>
2019-04-10 00:00:05 +0900

    feat: Modify Ice_Cream_Parlor.kt
    description:
    description:
    Given the price of each ice cream and the money you have, print out the kind of ice cream you can afford to spend all your money on.

--- Algorithms/kotlin/Search-Ice_Cream_Parlor.kt
+++ Algorithms/kotlin/Search-Ice_Cream_Parlor.kt
@@ -5,6 +5,13 @@ import kotlin.text.*
 
 fun icecreamParlor(m: Int, arr: Array<Int>): Array<Int> {
 
+    // 1. arr를 탐색하며 인덱스를 선택하고
+    (0 until arr.size).forEach {
+        // 1-1. 선택한 인덱스를 it라고 했을 때, m에서 arr[it]를 뺀 값이 arr의 나머지 부분에서 있는지 확인한다.
+        // 1-2. 있다면, it+1 값과 arr[it2] = m-arr[it]를 만족하는 it2+1 값을 출력한다.
+        if (m - arr[it] in arr.sliceArray(it+1 until arr.size)) return arrayOf(it+1, arr.sliceArray(it+1 until arr.size).indexOf(m - arr[it]) + it + 2)
+    }
+
     return arrayOf(0)
 }
 
