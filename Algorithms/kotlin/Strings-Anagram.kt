d02ab188de86e97f5d008e0b034aababceef0cb9
EthanShin <avoirom@gmail.com>
2019-04-12 23:42:06 +0900

    feat: Add Anagram.kt
    description:
    When dividing the input string into halves, how many characters should be changed to make the two strings created equal?
    
    Timeout #5, #7, #9, #11, #12, #13

--- Algorithms/kotlin/Strings-Anagram.kt
+++ Algorithms/kotlin/Strings-Anagram.kt
@@ -4,7 +4,22 @@ import kotlin.text.*
 
 fun anagram(s: String): Int {
 
-    return 0
+    // 1. 입력받은 문자열은 반으로 나누어 s1, s2에 할당한다.
+    val s1 = s.substring(0, s.length / 2)
+    var s2 = s.substring(s.length / 2, s.length)
+    var result = 0
+
+    // 2. 두 문자열의 길이가 다르면 -1을 출력한다.
+    if (s1.length != s2.length) return -1
+
+    // 3. s1의 값이 s2에 있다면 해당 값을 공백으로 변경한다.
+    // 3-1. 값이 없으면 result에 1을 더한다.
+    (0 until s.length/2).forEach {
+        if (s2.contains(s1[it])) s2 = s2.replaceFirst(s1[it], ' ')
+        else result++
+    }
+
+    return result
 }
 
 fun main(args: Array<String>) {
