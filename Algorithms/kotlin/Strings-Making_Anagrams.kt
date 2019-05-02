629bc4300d8ea1506815fdf8012177377b6b3a4c
EthanShin <avoirom@gmail.com>
2019-05-02 23:59:04 +0900

    feat: Modify Making_Anagrams.kt
    description:
    Given two strings, s1 and s2, that may not be of the same length, determine the minimum number of character deletions required to make s1 and s2 anagrams.

--- Algorithms/kotlin/Strings-Making_Anagrams.kt
+++ Algorithms/kotlin/Strings-Making_Anagrams.kt
@@ -1,9 +1,26 @@
 import java.util.*
 import kotlin.io.*
+import kotlin.math.abs
 
 fun makingAnagrams(s1: String, s2: String): Int {
 
-    return 0
+    var map = mutableMapOf<Char, Int>()
+    var result = 0
+
+    // 1. s1에 해당하는 값은 map에서 1씩 더한다.
+    (0 until s1.length).forEach {
+        map[s1[it]] = map.getOrDefault(s1[it], 0) + 1
+    }
+
+    // 2. s2에 해당하는 값은 map에서 1씩 뺀다.
+    (0 until s2.length).forEach {
+        map[s2[it]] = map.getOrDefault(s2[it], 0) - 1
+    }
+
+    // 3. map에서 value가 0이 아닌 값을 result에 더한다.
+    map.values.forEach { if (it != 0) result += abs(it) }
+
+    return result
 }
 
 fun main(args: Array<String>) {
