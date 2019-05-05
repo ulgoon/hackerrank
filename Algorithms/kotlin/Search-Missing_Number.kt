b11c70f04d39f1e65dd6ca69cd509c2882d2070e
EthanShin <avoirom@gmail.com>
2019-05-05 22:26:53 +0900

    feat: Modify Missing_Number.kt
    description:
    Compare the two arrays to determine the missing numbers.

--- Algorithms/kotlin/Search-Missing_Number.kt
+++ Algorithms/kotlin/Search-Missing_Number.kt
@@ -5,7 +5,25 @@ import kotlin.text.*
 
 fun missingNumbers(arr: Array<Int>, brr: Array<Int>): Array<Int> {
 
-    return arrayOf(0)
+    var map = mutableMapOf<Int, Int>()
+    var result = arrayListOf<Int>()
+
+    // 1. brr에 해당하는 값은 map에서 1씩 더한다.
+    (0 until brr.size).forEach {
+        map[brr[it]] = map.getOrDefault(brr[it], 0) + 1
+    }
+
+    // 2. arr에 해당하는 값은 map에서 1씩 뺀다.
+    (0 until arr.size).forEach {
+        map[arr[it]] = map.getOrDefault(arr[it], 0) - 1
+    }
+
+    // 3. map에서 value가 0보다 큰 값을 result에 더한다.
+    map.values.forEachIndexed { index, i ->
+        if (i > 0) { result.add(map.keys.elementAt(index)) }
+    }
+
+    return result.sorted().toTypedArray()
 }
 
 fun main(args: Array<String>) {
