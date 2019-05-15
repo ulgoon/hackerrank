7e4fdab4e8a2476a2bed6d891c07a044d75b0dfc
EthanShin <avoirom@gmail.com>
2019-05-15 20:15:04 +0900

    feat: Modify Luck_Balance.kt
    description:
    Lena is preparing for an important coding competition that is preceded
    by a number of sequential preliminary contests. She believes in "saving
    luck", and wants to check her theory.

--- Algorithms/kotlin/Greedy-Luck_Balance.kt
+++ Algorithms/kotlin/Greedy-Luck_Balance.kt
@@ -6,7 +6,27 @@ import kotlin.text.*
 
 fun luckBalance(k: Int, contests: Array<Array<Int>>): Int {
 
-    return 0
+    var impContest = mutableListOf<Int>()
+    var luck = 0
+
+    // 1. 대회 배열을 탐색한다.
+    contests.forEach {
+        // 1-1. 중요 대회 리스트를 뽑아낸다.
+        if (it[1] == 1) impContest.add(it[0])
+
+        // 1-2. 전체 운 값을 더한다.
+        luck += it[0]
+    }
+
+    // 2. 중요 대회 리스트를 오름차순으로 정렬한다.
+    impContest.sort()
+
+    // 3. 중요 대회 리스트 중 k 값을 제외한 만큼 리스트를 탐색한다.
+    (0 until impContest.size - k).forEach {
+        // 3-1. 전체 운 값에서 해당 운 값을 제외한다.
+        luck -= 2 * impContest[it] }
+
+    return luck
 }
 
 fun main(args: Array<String>) {
