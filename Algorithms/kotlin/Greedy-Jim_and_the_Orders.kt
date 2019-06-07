0242ce4499174fa0fb1ad63bdb3d4b8846c963e2
EthanShin <avoirom@gmail.com>
2019-06-07 17:27:12 +0900

    feat: Modify Jim_and_the_Orders.kt
    description:
    Jim's Burgers has a line of hungry customers. Orders vary in the time it takes to prepare them. Determine the order the customers receive their orders.

--- Algorithms/kotlin/Greedy-Jim_and_the_Orders.kt
+++ Algorithms/kotlin/Greedy-Jim_and_the_Orders.kt
@@ -7,7 +7,16 @@ import kotlin.text.*
 
 fun jimOrders(orders: Array<Array<Int>>): Array<Int> {
 
-    return arrayOf(0)
+    var serveTime = hashMapOf<Int, Int>()
+
+    // 1. orders의 index를 이용한 반복문을 수행한다.
+    orders.forEachIndexed { index: Int, ints: Array<Int> ->
+        // 1-1. 내부 배열의 값을 더해 1차원 배열로 만든다.
+        serveTime[index+1] = ints[0] + ints[1]
+    }
+
+    // 2. value를 기준으로 정렬하여 결과를 반환한다.
+    return serveTime.toList().sortedBy { (key, value) -> value }.toMap().keys.toTypedArray()
 }
 
 fun main(args: Array<String>) {
@@ -22,6 +31,5 @@ fun main(args: Array<String>) {
     }
 
     val result = jimOrders(orders)
-
     println(result.joinToString(" "))
 }
