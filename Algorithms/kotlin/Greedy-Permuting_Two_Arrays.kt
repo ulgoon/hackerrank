94755f694493bedd8a2bf9bf15031f2bf8c1aacd
EthanShin <avoirom@gmail.com>
2019-06-09 12:48:29 +0900

    feat: Modify Permuting_Two_Arrays.kt
    description:
    You are given q queries consisting of A, B, and k. For each query, print YES on a new line if some permutation A', B' satisfying the relation above exists. Otherwise, print NO.

--- Algorithms/kotlin/Greedy-Permuting_Two_Arrays.kt
+++ Algorithms/kotlin/Greedy-Permuting_Two_Arrays.kt
@@ -3,8 +3,20 @@ import kotlin.collections.*
 import kotlin.io.*
 
 fun twoArrays(k: Int, A: Array<Int>, B: Array<Int>): String {
-µ
-    return ""
+
+    // 1. A를 오름차순으로, B를 내림차순으로 정렬한다.
+    A.sort()
+    B.sortDescending()
+
+    // 2. 같은 인덱스에 해당하는 값을 더하여 k와 비교한다.
+    for (i in 0 until A.size) {
+        if(A[i] + B[i] < k) {
+            // 2-1. 더한 값이 k보다 작으면 "NO"를 반환한다.
+            return "NO"
+        }
+    }
+    // 2-2. 그렇지않으면 "YES"를 반환한다.
+    return "YES"
 }
 
 fun main(args: Array<String>) {
