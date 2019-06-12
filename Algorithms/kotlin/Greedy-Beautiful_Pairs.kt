d2b1763cc663db864f6a278a26681a4f0b005847
EthanShin <avoirom@gmail.com>
2019-06-12 21:36:26 +0900

    feat: Modify Beautiful_Pairs.kt
    description:
    Print out how many times the same value appears in both arrays.

--- Algorithms/kotlin/Greedy-Beautiful_Pairs.kt
+++ Algorithms/kotlin/Greedy-Beautiful_Pairs.kt
@@ -5,7 +5,26 @@ import kotlin.text.*
 
 fun beautifulPairs(A: Array<Int>, B: Array<Int>): Int {
 
-    return 0
+    var map = mutableMapOf<Int, Int>()
+    var result = 0
+
+    // 1. brr에 해당하는 값은 map에서 1씩 더한다.
+    (0 until A.size).forEach {
+        map[A[it]] = map.getOrDefault(A[it], 0) + 1
+    }
+
+    // 2. arr에 해당하는 값은 map에서 1씩 뺀다.
+    (0 until B.size).forEach {
+        if (map.getOrDefault(B[it],0) > 0) {
+            map[B[it]] = map.getValue(B[it]) - 1
+            result++
+        }
+    }
+
+    // 3. 반드시 하나의 값을 변경해야하므로 A.size보다 작은 결과를 가지면 + 1
+    // 그렇지 않으면 -1의 결과를 반환한다.
+    return if (result < A.size) result + 1
+    else result - 1
 }
 
 fun main(args: Array<String>) {
