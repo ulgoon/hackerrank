693dffee8437501917ba52dbcab5f58ebf89c387
EthanShin <avoirom@gmail.com>
2019-06-18 22:56:04 +0900

    feat: Modify Maximizing_XOR.kt
    description:
    Given two integers, L and R, find the maximal value of A xor B, where A and B satisfy a condition.

--- Algorithms/kotlin/Bit_Manipulation-Maximizing_XOR.kt
+++ Algorithms/kotlin/Bit_Manipulation-Maximizing_XOR.kt
@@ -4,7 +4,14 @@ import kotlin.text.*
 
 fun maximizingXor(l: Int, r: Int): Int {
 
-    return 0
+    // 1. l과 r의 xor값을 구한다.
+    var value = l xor r
+    // 2. 비트값으로 변경하여 '1'이전에 0의 개수를 구한다. (32비트 기준)
+    var startbit = 32 - Integer.numberOfLeadingZeros(value)
+    // 3. 1을 startbit만큼 왼쪽으로 시프트한 뒤, 1을 뺀 값이 최대값이다.
+    var result = 1.shl(startbit) - 1
+
+    return result
 }
 
 fun main(args: Array<String>) {
