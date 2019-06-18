265b525360bfde7e6c4397647508c1b3d537d838
EthanShin <avoirom@gmail.com>
2019-06-18 22:23:59 +0900

    feat: Add Maximizing_XOR.kt
    description:
    Given two integers, L and R, find the maximal value of A xor B, where A
    and B satisfy a condition.

---
+++ Algorithms/kotlin/Bit_Manipulation-Maximizing_XOR.kt
@@ -0,0 +1,18 @@
+import java.util.*
+import kotlin.io.*
+import kotlin.text.*
+
+fun maximizingXor(l: Int, r: Int): Int {
+
+    return 0
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val l = scan.nextLine().trim().toInt()
+    val r = scan.nextLine().trim().toInt()
+
+    val result = maximizingXor(l, r)
+    println(result)
+}
