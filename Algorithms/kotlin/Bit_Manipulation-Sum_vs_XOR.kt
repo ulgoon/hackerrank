521d877d5eeb9edc6ade73d0969d23a9b0afe919
EthanShin <avoirom@gmail.com>
2019-06-19 23:14:25 +0900

    feat: Add Sum_vs_XOR.kt
    description:
    Find all the numbers below n such that the sum and xor of that number with n are the same.

---
+++ Algorithms/kotlin/Bit_Manipulation-Sum_vs_XOR.kt
@@ -0,0 +1,14 @@
+import kotlin.io.*
+import kotlin.text.*
+
+fun sumXor(n: Long): Long {
+
+    return 0
+}
+
+fun main(args: Array<String>) {
+    val n = readLine()!!.trim().toLong()
+
+    val result = sumXor(n)
+    println(result)
+}
