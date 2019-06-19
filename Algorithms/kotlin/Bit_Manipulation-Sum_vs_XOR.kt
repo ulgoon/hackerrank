8a671be1030ee34d07943a97c4b2322d03acad88
EthanShin <avoirom@gmail.com>
2019-06-20 00:29:11 +0900

    feat: Modify Sum_vs_XOR.kt
    description:
    Find all the numbers below n such that the sum and xor of that number with n are the same.

--- Algorithms/kotlin/Bit_Manipulation-Sum_vs_XOR.kt
+++ Algorithms/kotlin/Bit_Manipulation-Sum_vs_XOR.kt
@@ -3,7 +3,22 @@ import kotlin.text.*
 
 fun sumXor(n: Long): Long {
 
-    return 0
+    var value = n
+    var result:Long = 1
+
+    // 1. value를 2로 나누면서 1보다 클 경우까지 반복하여 아래의 코드를 수행한다.
+    while (value > 1) {
+        // 2. value를 2로 나눈 나머지가 0이면 현재 마지막 비트가 0이라는 것을 의미한다.
+        if (value.rem(2).toInt() == 0) {
+            // 3. 0이 나오는 비트의 개수를 2의 지수로 가지는 결과를 얻을 수 있다.
+            result *= 2
+        }
+
+        // 4. 오른쪽 쉬프트는 2로 나누는 것과 같은 결과를 갖는다.
+        value = value.shr(1)
+    }
+
+    return result
 }
 
 fun main(args: Array<String>) {
