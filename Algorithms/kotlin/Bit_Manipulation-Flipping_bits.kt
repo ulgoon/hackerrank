3af0621d937c371816fce20d6f2b0b2016dfa546
EthanShin <avoirom@gmail.com>
2019-06-20 17:53:35 +0900

    feat: Modify Flipping_bits.kt
    description:
    Flip bits in its binary representation.

--- Algorithms/kotlin/Bit_Manipulation-Flipping_bits.kt
+++ Algorithms/kotlin/Bit_Manipulation-Flipping_bits.kt
@@ -4,7 +4,8 @@ import kotlin.text.*
 
 fun flippingBits(n: Long): Long {
 
-    return 0
+    // 1. 2의 32승 값에 -1을 하여 32bit 모두 1을 갖는 수와 xor 연산을 한다.
+    return n.xor((Math.pow(2.0, 32.0) - 1).toLong())
 }
 
 fun main(args: Array<String>) {
