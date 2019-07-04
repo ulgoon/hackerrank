f5a981c3049d4b468384c20d3beaf129422ca974
EthanShin <avoirom@gmail.com>
2019-07-04 23:35:14 +0900

    feat: Modify Introduction_to_Nim_Game.kt
    description:
    Nim is the most famous two-player algorithm game. For each game, print the name of the winner on a new line.

--- Algorithms/kotlin/Game_Theory-Introduction_to_Nim_Game.kt
+++ Algorithms/kotlin/Game_Theory-Introduction_to_Nim_Game.kt
@@ -5,7 +5,14 @@ import kotlin.text.*
 
 fun nimGame(pile: Array<Int>): String {
 
-    return "First"
+    // 1. Nim 게임의 공략법인 균형상태를 확인한다.
+    // 2. pile의 요소를 모두 xor하여 값이 0이 나오면 균형상태이므로, Second가 승리한다.
+    pile.reduce { acc, i -> acc xor i }.let {
+        return when(it) {
+            0 -> "Second"
+            else -> "First"
+        }
+    }
 }
 
 fun main(args: Array<String>) {
