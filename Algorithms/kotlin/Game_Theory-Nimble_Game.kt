e6c3e34b933af2e7d417cd7dd0b5f3a4678ae982
EthanShin <avoirom@gmail.com>
2019-07-07 02:02:45 +0900

    feat: Modify Nimble_Game.kt
    description:
    Given the value of n and the number of coins in each square, determine whether the person who wins the game is the first or second person to move. Assume both players move optimally.

--- Algorithms/kotlin/Game_Theory-Nimble_Game.kt
+++ Algorithms/kotlin/Game_Theory-Nimble_Game.kt
@@ -5,7 +5,18 @@ import kotlin.text.*
 
 fun nimbleGame(s: Array<Int>): String {
 
-    return "First"
+    var nim = 0
+
+    // 1. 돌의 개수가 홀수인 인덱스 값을 xor하여 균형상태인지 확인한다.
+    s.forEachIndexed { index, i ->
+        if ( i % 2 == 1) {
+            nim = nim.xor(index)
+        }
+    }
+
+    // 2. 균형상태라면 Second가 승리한다.
+    return if (nim == 0) "Second"
+    else "First"
 }
 
 fun main(args: Array<String>) {
